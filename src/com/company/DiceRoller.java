bd162178789560f62a0902a5f0ebc9b865b844b4
Diwakar Sharma <diwakar1@umbc.edu>
2018-06-01 11:41:14 -0400

    first Commit

---
+++ src/com/company/DiceRoller.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+public class DiceRoller {
+    public static void main(String[] args) {
+        int dienumber;
+        Random rnd = new Random();
+
+        dienumber = rnd.nextInt(6)+1;
+
+        System.out.println("Your die roll was : " + dienumber);
+    }
+}
